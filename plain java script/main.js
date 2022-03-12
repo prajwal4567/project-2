c99fbb2f3be1eed7c39ca8e6cb0b5c3ea5467a1b
prajwal4567 <82152156+prajwal4567@users.noreply.github.com>
2022-03-12 18:36:13 +0530

    Add files via upload

--- plain java script/main.js
+++ plain java script/main.js
@@ -1,22 +1,45 @@
-let block1,block2,block3,block4,block5,i;
+var red,blue;
+var a=[];
+var k=[];
+var ac=[];
+var kc=[];
+var allvalue=[];
+var pairstore=[];
+var m;
+function preload(){
+    red = loadImage("blue_circle2.png");
+    blue = loadImage("blue_circle1.png");
+}
 function setup(){
     createCanvas(1000,1000);
-    for(i = 0 ; i<6 ;i++){
-        a= new Array(0+i);
-        a=createSprite(Math.round(random(0,1000)),Math.round(random(0,1000)),10,10);
-        console.log(a[1]);
+    for(var i = 0 ; i<21 ;i++){
+        a[i]=createSprite(Math.round(random(0,1000)),Math.round(random(0,1000)),10,10);
+        a[i].addImage("blue",blue);
+        ac.push({x:a[i].x,y:a[i].y});
     }
+    for(var f = 0 ; f<6 ;f++){
+        k[f]=createSprite(Math.round(random(0,1000)),Math.round(random(0,1000)),10,10);
+        k[f].addImage("red",red);
+        kc.push({x:k[f].x,y:k[f].y});
+    }
+    allvalue.push(ac,kc)
+    pairstore.push({x:allvalue[0][0].x,y:allvalue[0][0].y,ax:allvalue[1][0].x,ay:allvalue[1][0].x});
 }
 function draw(){
     background("black");
+    //var k=repel();
+    var n=attract(pairstore[0].x,pairstore[0].y,pairstore[0].ax,pairstore[0].ay);
     drawSprites();
+    a[0].x=n[0];
+    a[0].y=n[1]; 
+    console.log(a[0].x);             
 }  
-function attarct(x,y,ax,ay){
-let distanceax=x-ax;
-let distanceay=y-ay;
-let directionax=Math.sign(distanceax);
-let directionay=Math.sign(distanceay);
-let changeax,changeay;
+function attract(x,y,ax,ay){
+var distanceax=x-ax;
+var distanceay=y-ay;
+var directionax=Math.sign(distanceax);
+var directionay=Math.sign(distanceay);
+var changeax,changeay;
     if(directionax = 1){
         changeax=100/distanceax;
         ax = ax+changeax;
@@ -39,13 +62,15 @@ let changeax,changeay;
     if(distanceax=0){
         ax =ax+ 0;
     }
+    var p=[ax,ay];
+    return p;
 }
-function repail(x,y,ax,ay){
-let distanceax=x-ax;
-let distanceay=y-ay;
-let directionax=Math.sign(distanceax);
-let directionay=Math.sign(distanceay);
-let changeax,changeay;
+function repel(x,y,ax,ay){
+var distanceax=x-ax;
+var distanceay=y-ay;
+var directionax=Math.sign(distanceax);
+var directionay=Math.sign(distanceay);
+var changeax,changeay;
     if(distanceax <101 || distanceay<101 || distanceax <-101 || distanceay<-101){
         if(directionax = 1){
             changeax=100-distanceax;
@@ -64,4 +89,6 @@ let changeax,changeay;
             ay = ay-changeay;
         }
     }
+var p=[ax,ay];
+return p;
 } 
